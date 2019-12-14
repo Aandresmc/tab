1347d231883940ef85a4a5202fc9e2667d1a2905
Andres Mora Castro <llaandresmcll@gmail.com>
2019-12-14 12:40:56 -0500

    creacion de tabs y datos generales pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TabsModule } from './core/tabs/tabs.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
